8a2f5bd35b12ba7e3957001e2eb5a3b63eb58252
ArjunKsanthosh <arjunk80043@gmail.com>
2024-08-13 19:36:37 +0530

    Addddd

--- Js/cart.js
+++ Js/cart.js
@@ -1,11 +1,11 @@
 function getCart(){
     str=``
+    str1=``
     for(i=0;i<localStorage.length;i++){
         const key=localStorage.key(i)
         const value=JSON.parse(localStorage.getItem(key))
         console.log(value);
         str+=`
-
         <div class="cards">
 
             <div class="cimg1">
@@ -18,7 +18,11 @@ function getCart(){
                 <p> <span class="f1">Price:</span> $ ${value.price}</p>
                 <p class="f2">${value.description.substring(0,90)}</p>
             </div>
-
+  <p>
+                        <span class="qnty" onclick="minus('${i}')"><img src="./img/plus.png" alt=""></span>
+                        <span id="txt${i}" class="txt"></span>
+                        <span class="qnty" onclick="plus('${i}')"><img src="./img/minus.png" alt=""></span>
+                    </p>
 
 
             <div class="btrn1">
@@ -27,9 +31,14 @@ function getCart(){
 
             </div>
 
-        </div>`
+        </div> `
+      
+        
+          
     }
     document.getElementById("add").innerHTML=str
+    document.getElementById("ad").innerHTML=str1   
+
 }
 getCart();
 
